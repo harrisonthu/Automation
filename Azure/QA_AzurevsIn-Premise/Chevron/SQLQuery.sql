b3600098c5775339e78a9945b9ca62794613b800
Han Thu <hmt4224@g.rit.edu>
2019-01-15 15:21:46 -0500

    Added file necessary to QA

---
+++ Azure/QA_AzurevsIn-Premise/Chevron/SQLQuery.sql
@@ -0,0 +1,52 @@
+  
+				-------------------------------------- Azure ---------------------------
+                SELECT MAX([Day])
+                FROM [dbo].[DFID066573_DS999922_Email_Innovid_Device_Video_Report_Chevron_Extracted]
+
+				
+                SELECT count(*)
+                FROM [dbo].[DFID066573_DS999922_Email_Innovid_Device_Video_Report_Chevron_Extracted]
+
+
+				                SELECT count(*)
+                FROM [dbo].[DFID066576_DS999923_Email_Innovid_Device_Metadata_Report_Chevron_Extracted]
+
+
+				                SELECT MAX([Date])
+                FROM [dbo].[DFID066577_DS020307_Sizmek_Device_Video_Report_Chevron_Extracted]
+
+
+				                SELECT count(*)
+                FROM [dbo].[DFID066577_DS020307_Sizmek_Device_Video_Report_Chevron_Extracted]
+
+				PRINT(GETDATE())
+
+				
+				----------------------------------  PREMISE
+
+                SELECT MAX([Day])
+                FROM [dbo].[DFID066573_DS999922_Email_Innovid_Device_Video_Report_Chevron_Extracted]
+
+                SELECT count(*)
+                FROM [dbo].[DFID066573_DS999922_Email_Innovid_Device_Video_Report_Chevron_Extracted]
+
+
+				
+					SELECT count(*)
+					FROM [dbo].[DFID066576_DS999923_Email_Innovid_Device_Metadata_Report_Chevron_Extracted]
+
+					SELECT MAX([Date])
+					FROM [dbo].[DFID066577_DS020307_Sizmek_Device_Video_Report_Chevron_Extracted]
+
+					SELECT count(*)
+					FROM [dbo].[DFID066577_DS020307_Sizmek_Device_Video_Report_Chevron_Extracted]
+
+					
+					PRINT(GETDATE())
+
+
+
+
+
+
+
