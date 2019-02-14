fbd57634daa3def0eacc8cfe54a42834ea329612
Han Thu <hmt4224@g.rit.edu>
2019-02-14 16:57:49 -0500

    Added FindMissingGaps python file
    Added the python script that will find any missing data (missing dates) for three tables (IAS, Sizmek, Adobe/Omniture), and give out the results.
    
    TODO:
     - include "Sizmek table" function
     - send out the result through email/Python UI

---
+++ findMissinggaps/FindMissingGaps.py
@@ -0,0 +1,125 @@
+### Author:     Han Thu  	  ###
+### Date:       February 06, 2019  ###
+### Client:     T Rowe Price (DM_1406,  10.2.186.148\SQLINS02) ###
+### Description:
+### Finding the missing data gaps in the three tables names below ###
+### Print out the missing dates' data  ###
+### Push out in excel/csv format categorizing sheets according to three data sources below ###
+###      - (IAS)      [DFID052284_DS999622_FTP_IAS_TRP_Extracted]
+###      - (Adobe)    [DFID052226_DS999621_Email_Omniture_TRP_US_Extracted]
+###      - (Sizmek)   [DFID061542_DS020301_Sizmek_Campaign_Report_TRowePrice_Extracted]
+
+
+#!/usr/bin/env python     ## Set interpreter used to be the one on your environment's $PATH
+#import pyodbc
+import time
+import os
+import datetime
+import xlsxwriter
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+"""
+  Function to connect the Server Name
+  so that the other function can access and use the connection
+"""
+def Connection():
+  Connection.ServerName = "10.2.186.148\SQLINS02"
+  Connection.Database = "DM_1406_TRowePrice"
+  Connection.UserPwd = "DASuser01:DASpassw0rd"
+  Connection.Driver = "driver=SQL Server Native Client 11.0"
+  # Create the connection
+  print('Connecting ' + Connection.Database + ' Database ...')
+  Connection.engine = create_engine('mssql+pyodbc://' + Connection.UserPwd + '@' + Connection.ServerName + '/' + Connection.Database + "?" + Connection.Driver)
+
+
+
+"""
+    Checking data gaps from Jan 01, 2019 from  the table is missing:
+    Retrieving data and add it in 
+"""
+def getIASTable():
+  """
+  Function to pull the table From In-Premise table
+  using Panda library(read_sql) 
+  """
+  
+  maxsql = """
+        SELECT MAX([Date])
+        FROM [DM_1406_TRowePrice].[dbo].[DFID052284_DS999622_FTP_IAS_TRP_Extracted]
+        """
+
+  ##  Connecting IAS Table (DS999622_FTP_IAS_TRP)
+  ##  List out all the missing dates
+  sql = """
+        SELECT
+        DayInBetween AS missingDate
+        FROM dbo.GetAllDaysInBetween('2019-01-01', 
+	----  Find the Max date in the table/view
+	(SELECT MAX([Date])
+        FROM [DM_1406_TRowePrice].[dbo].[DFID052284_DS999622_FTP_IAS_TRP_Extracted])
+	) AS AllDaysInBetween
+	----  END: Find the Max date in the table/view
+        WHERE NOT EXISTS 
+        (SELECT [adserver id] FROM [DM_1406_TRowePrice].[dbo].[DFID052284_DS999622_FTP_IAS_TRP_Extracted] WHERE [Date] = AllDaysInBetween.DayInBetween)
+        """
+  print("Connected ", Connection.Database)
+  print("Finding Missing data gaps in Table [DFID052284_DS999622_FTP_IAS_TRP_Extracted] ...")
+  then = time.time() # Time before the operations start
+  IASdf = pd.read_sql(sql, Connection.engine)  # store data frame type of the result
+  now = time.time()  # Time after it finished
+  maxdateIAS = pd.read_sql(maxsql, Connection.engine)
+  #df['DOB1'] = df['DOB'].dt.strftime('%m/%d/%Y')
+  #print("Max date for Table [DFID052284_DS999622_FTP_IAS_TRP_Extracted]: "+ maxdateIAS.to_datetime(datetime))
+  print("Max date for Table [DFID052284_DS999622_FTP_IAS_TRP_Extracted]: ", maxdateIAS.iloc[0,0])
+  print("Finished finding missing data gaps. It took: ",int(now-then), " seconds\n")
+  return IASdf
+
+
+def getAdobeTable():
+  """
+  Function to pull the table From In-Premise table
+  using Panda library(read_sql) 
+  """
+  maxsql_adobe = """
+        SELECT MAX([Day])
+        FROM [DM_1406_TRowePrice].[dbo].[DFID052226_DS999621_Email_Omniture_TRP_US_Extracted]
+        """
+
+  ##  Connecting Adobe Table (DS999622_FTP_IAS_TRP)
+  ##  List out all the missing dates
+  sql_adobe = """
+        SELECT
+        DayInBetween AS missingDate
+        FROM dbo.GetAllDaysInBetween('2019-01-01', 
+	----  Find the Max date in the table/view
+	(SELECT MAX([Day])
+        FROM [DM_1406_TRowePrice].[dbo].[DFID052226_DS999621_Email_Omniture_TRP_US_Extracted])
+	) AS AllDaysInBetween
+	----  END: Find the Max date in the table/view
+        WHERE NOT EXISTS 
+        (SELECT [Visits] FROM [DM_1406_TRowePrice].[dbo].[DFID052226_DS999621_Email_Omniture_TRP_US_Extracted] WHERE [Day] = AllDaysInBetween.DayInBetween)
+         """
+  print("Finding Missing data gaps in Table [DFID052226_DS999621_Email_Omniture_TRP_US_Extracted] ...")
+  then = time.time() # Time before the operations start
+  Adobedf = pd.read_sql(sql_adobe, Connection.engine)  # store data frame type of the result
+  now = time.time()  # Time after it finished
+  maxdateAdobe = pd.read_sql(maxsql_adobe, Connection.engine)
+  print("Max date for Table [DFID052226_DS999621_Email_Omniture_TRP_US_Extracted]: ", maxdateAdobe.iloc[0,0])
+  print("Finished finding missing data gaps. It took: ",int(now-then), " seconds")
+  return Adobedf
+
+
+
+def getAdobeTable():
+  
+
+
+def main():
+  Connection()
+  getIASTable()
+  getAdobeTable()
+
+
+main()
